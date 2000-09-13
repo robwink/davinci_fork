0f6c7978dc266cecdae9b7fa622591a05a91b995
gorelick <gorelick@dedd6328-c50f-0410-8be5-b54a49638459>
2000-09-13 22:42:49 +0000

    Added _replace functions

--- davinci/trunk/darray.c
+++ davinci/trunk/darray.c
@@ -17,6 +17,10 @@
  **
  ** 	Replace item at index with new value.  Retrieves old value.
  ** 	Returns 1 on success.
+ **
+ ** int Darray_free( array, void (*function_pointer)()) 
+ **
+ **     Free the elements in a Darray using the specified function pointer
  **/
 
 /**
@@ -25,37 +29,41 @@
  ** An array whose elements can be indexed by integer or by an associated
  ** key (string).
  **
- **
- ** Aarray * Aarray_create(int size)
+ ** Narray * Narray_create(int size)
  **
  ** 	Create an associative array with an estimated size.
  **     It is ok to pass 0 for size.
  **
- ** int Aarray_add(Aarray *a, char *key, void *data)
+ ** int Narray_add(Narray *a, char *key, void *data)
  **
  ** 	Append data to the end of the array and add key to the lookup table.
  ** 	Returns the element's index in the array on success or -1 on error.
  **     Will fail if key already exists.
  **
- ** int Aarray_find(Aarray *a, char *key, void **data)
+ ** int Narray_find(Narray *a, char *key, void **data)
  **
  ** 	Find the element with associated key.  Get its data and
  ** 	return its index on success.  Ok to pass a NULL for data.
  **     Fails if key doesn't exist.  Returns -1 on error.
  **
- ** int Aarray_get(Aarray *a, int index, char **key, void **data)
+ ** int Narray_get(Narray *a, int index, char **key, void **data)
  **
  ** 	Get the key and data of the element at index.
  **     Ok to pass either key or data as NULL.
  ** 	Returns 1 on success, -1 on error.
  **
- ** int Aarray_count(Aarray *a)
+ ** int Narray_count(Narray *a)
  **
  ** 	Returns number of elements in array, or -1 on error.
+ **
+ ** int Narray_replace_data(Narray *a, int i, void *new, void **old)
+ **
+ **    Replace the data element at index i, returning the old one
  **/
 
 #include <stdio.h>
 #include <stdarg.h>
+#include "darray.h"
 
 /*
 ** Darray_create(size)
@@ -150,6 +158,18 @@ int Darray_count(Darray *d)
     return(-1);
 }
 
+void Darray_free(Darray *d, void (*fptr)()) 
+{
+	int i;
+	if (fptr) {
+		for (i = 0 ; i < d->count ; i++) {
+			if (d->data[i]) fptr(d->data[i]);
+		}
+	}
+	free(d->data);
+	free(d);
+}
+
 /*
 ****************************************************************************
 ** Associative Array
@@ -160,30 +180,38 @@ typedef struct _anode {
     int index;
     char *key;
     void *value;
-} Anode;
+} Nnode;
 
 
 int Acmp(const void *a, const void *b, void *param)
 {
-    return(strcmp(((Anode *)a)->key, ((Anode *)b)->key));
+    return(strcmp(((Nnode *)a)->key, ((Nnode *)b)->key));
 }
 
-Anode *
-Anode_create(char *key, void *value)
+Nnode *
+Nnode_create(char *key, void *value)
 {
-    Anode *a = (Anode *)calloc(1, sizeof(Anode));
-    a->key = key;
+    Nnode *a = (Nnode *)calloc(1, sizeof(Nnode));
+    a->key = (char *)strdup(key);
     a->value = value;
     a->index = -1;      /* unassigned */
     return(a);
 }
 
-
-Aarray *
-Aarray_create(int size)
+void
+Nnode_free(Nnode *a, void (*fptr)())
 {
-    Aarray *a;
-    a = (Aarray *)calloc(1, sizeof(Aarray));
+	if (a->key) free(a->key);
+	if (fptr) fptr(a->value);
+	free(a);
+}
+
+
+Narray *
+Narray_create(int size)
+{
+    Narray *a;
+    a = (Narray *)calloc(1, sizeof(Narray));
     a->data = Darray_create(size);
     a->tree = avl_create(Acmp, NULL);
     return(a);
@@ -201,16 +229,16 @@ Aarray_create(int size)
 **        -1 on failure
 */
 int
-Aarray_add(Aarray *a, char *key, void *data)
+Narray_add(Narray *a, char *key, void *data)
 {
     char *r;
-    Anode *n;
+    Nnode *n;
 
     if (a == NULL) return(-1);
     /*
     ** See if this key already exists
     */
-    n = Anode_create(key, data);
+    n = Nnode_create(key, data);
     
     if (key) {
         r = avl_insert(a->tree, n);
@@ -218,7 +246,7 @@ Aarray_add(Aarray *a, char *key, void *data)
             /*
             ** Key already exists.  Abort.
             */
-            free(n);
+            Nnode_free(n, NULL);
             return(-1);
         }
     }
@@ -234,10 +262,10 @@ Aarray_add(Aarray *a, char *key, void *data)
 ** otherwise, returns -1.
 */
 int
-Aarray_find(Aarray *a, char *key, char **data)
+Narray_find(Narray *a, char *key, void **data)
 {
-    Anode n;
-    Anode *found;
+    Nnode n;
+    Nnode *found;
 
     n.key = key;
     
@@ -252,12 +280,30 @@ Aarray_find(Aarray *a, char *key, char **data)
 }
 
 /*
-** Aarray_get() - Get values of element at index.
+**  Narray_replace_data
+**
+**  Replace just the data at index i, returning the old data
+*/
+
+int
+Narray_replace_data(Narray *a, int i, void *new, void **old)
+{
+    Nnode *n;
+	if (Darray_get(a->data, i, (void **)&n) == 1) {
+		*old = n->value;
+		n->value = new;
+		return(1);
+	}
+	return(-1);
+}
+
+/*
+** Narray_get() - Get values of element at index.
 */
 int
-Aarray_get(Aarray *a, int i, char **key, void **data)
+Narray_get(Narray *a, int i, char **key, void **data)
 {
-    Anode *n;
+    Nnode *n;
     if (Darray_get(a->data, i, (void **)&n) == 1) {
         if (key) *key = n->key;
         if (data) *data = n->value;
@@ -267,10 +313,25 @@ Aarray_get(Aarray *a, int i, char **key, void **data)
 }
 
 int 
-Aarray_count(Aarray *a)
+Narray_count(Narray *a)
 {
     if (a) return(Darray_count(a->data));
     return(-1);
+}
+
+void
+Narray_free(Narray *a, void (*fptr)())
+{
+	int i;
+	int count = Darray_count(a->data);
+	Nnode *n;
+
+	for (i = 0 ; i < count ; i++) {
+		Darray_get(a->data, i, (void **)&n);
+		Nnode_free(n, fptr);
+		Darray_free(a->data, NULL);
+	}
+	avl_free(a->tree);
 }
 
 #ifdef TEST
@@ -284,10 +345,10 @@ int Fail(char *fmt, ...)
     exit(-1);
 }
 
-test_Aarray()
+test_Narray()
 {
     int i;
-    Aarray *a;
+    Narray *a;
     int r_i;
     void *r_v;
     int d[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -297,35 +358,35 @@ test_Aarray()
     char *key;
     int *data;
 
-    a = Aarray_create(0);
+    a = Narray_create(0);
 
-    r_i = Aarray_find(a, "one", NULL);
+    r_i = Narray_find(a, "one", NULL);
     if (r_i != -1) Fail("find on nonexistant.\n");
 
     for (i = 0 ; i < 10 ; i++) {
-        r_i = Aarray_add(a, k[i], &d[i]);
+        r_i = Narray_add(a, k[i], &d[i]);
         if (r_i < 0) Fail("addition of element: %d\n", i);
     }
 
     for (i = 0 ; i < 10 ; i++) {
-        r_i = Aarray_add(a, k[0], &d[0]);
+        r_i = Narray_add(a, k[0], &d[0]);
         if (r_i > 0) Fail("addition of existing element: %d\n", i);
     }
 
     for (i = 0 ; i < 10 ; i++) {
-        r_i = Aarray_add(a, NULL, &d[i]);
+        r_i = Narray_add(a, NULL, &d[i]);
     }    
 
     for (i = 9 ; i >= 0 ; i--) {
-        r_i = Aarray_find(a, k[i], (void *)&data);
-        printf("Aarray[%d] (%s) = %d\n", r_i, k[i], *data);
+        r_i = Narray_find(a, k[i], (void *)&data);
+        printf("Narray[%d] (%s) = %d\n", r_i, k[i], *data);
     }
 
     printf("\n");
 
-    for (i = 0 ; i < Aarray_count(a) ; i++) {
-        Aarray_get(a, i, &key, (void *)&data);
-        printf("Aarray[%d] (%s) = %d\n",
+    for (i = 0 ; i < Narray_count(a) ; i++) {
+        Narray_get(a, i, &key, (void *)&data);
+        printf("Narray[%d] (%s) = %d\n",
                i,
                (key == NULL ?  "(null)" : key),
                *data);
@@ -334,5 +395,5 @@ test_Aarray()
     
 }
 
-main() { test_Aarray(); }
+main() { test_Narray(); }
 #endif
--- davinci/trunk/darray.h
+++ davinci/trunk/darray.h
@@ -6,7 +6,21 @@ typedef struct _Darray {
     void **data;
 } Darray;
 
-typedef struct _Aarray {
+typedef struct _Narray {
     Darray *data;
     avl_tree *tree;
-} Aarray;
+} Narray;
+
+Darray * Darray_create(int size);
+int Darray_add(Darray *d, void *new);
+int Darray_get(Darray *d, int i, void **ret);
+int Darray_replace(Darray *d, int i, void *in, void **out);
+int Darray_count(Darray *d);
+void Darray_free(Darray *d, void (*fptr)()) ;
+Narray * Narray_create(int size);
+int Narray_add(Narray *a, char *key, void *data);
+int Narray_find(Narray *a, char *key, void **data);
+int Narray_replace_data(Narray *a, int i, void *new, void **old);
+int Narray_get(Narray *a, int i, char **key, void **data);
+int Narray_count(Narray *a);
+void Narray_free(Narray *a, void (*fptr)());
