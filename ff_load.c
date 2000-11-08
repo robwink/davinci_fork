58c2eb0cb5840c8f9e38951e7e513cea5d1727cf
asbms <asbms@dedd6328-c50f-0410-8be5-b54a49638459>
2000-11-08 21:02:18 +0000

    Added raw read to ff_load and better summary statistics to load_paci

--- ff_load.c
+++ ff_load.c
@@ -65,6 +65,55 @@ locate_file(char *fname)
     return (NULL);
 }
 
+Var *
+ff_raw(vfuncptr func, Var * arg)
+{
+	unsigned char *buf;
+	char *filename=NULL;
+	int Row=0;
+	int Col=0;
+	int size;
+	FILE *fp;
+	int ac;
+	Var **av;
+	Alist alist[4];
+	alist[0] = make_alist( "filename",  ID_STRING,  NULL,     &filename);
+	alist[1] = make_alist( "row", INT,NULL,&Row);
+	alist[2] = make_alist( "col", INT,NULL,&Col);
+	alist[3].name = NULL;
+
+	make_args(&ac, &av, func, arg);
+        if (parse_args(ac, av, alist)) return(NULL);
+
+        if (filename == NULL) {
+        	parse_error("No filename specified to load()");
+        	return (NULL);
+        }
+
+	if ((fp=fopen(filename,"r"))==NULL){
+		parse_error("Can't find file: %s",filename);
+		return(NULL);
+	}
+
+	if (!(Row) || !(Col)) {
+		parse_error("Must specify row and collumn size!");
+		return(NULL);
+	}
+
+	buf=(unsigned char *)calloc((Row*Col),sizeof(char));
+
+	size=fread(buf,sizeof(char),(Row*Col),fp);
+	if (size < (Row*Col)){
+		parse_error("Incorrect Row/Col size, sorry...aborting");
+		return(NULL);
+	}
+
+	return(newVal(BSQ,Col,Row,1,BYTE,buf));
+}
+
+
+
+
 
 Var *
 ff_load(vfuncptr func, Var * arg)
