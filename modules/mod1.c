a827a14a08833c40e0ca1f3a7f28e8b1a7b8aa77
gorelick <gorelick@dedd6328-c50f-0410-8be5-b54a49638459>
2001-01-17 19:03:55 +0000

    Adding modules and some missed windows files

---
+++ modules/mod1.c
@@ -0,0 +1,56 @@
+#include "parser.h"
+#include "ff_modules.h"
+
+
+static Var *foo(vfuncptr f, Var *args);
+static Var *bar(vfuncptr f, Var *args);
+
+static dvModuleFuncDesc exported_list[] = {
+    {"jack", (void *)foo},
+    {"sam", (void *)bar}
+};
+
+static dvModuleInitStuff is = {
+    exported_list, 2,
+    NULL, 0
+};
+
+DV_MOD_EXPORT int
+dv_module_init(
+    const char *name,
+    dvModuleInitStuff *init_stuff
+    )
+{
+	parse_error("*******************************************************");
+	parse_error("*******   mod1.c initialized with %-10s   ********", name);
+	parse_error("*******************************************************");
+
+    *init_stuff = is;
+    
+    return 1; /* return initialization success */
+}
+
+static Var *
+foo(vfuncptr f, Var *args)
+{
+    parse_error("%s:%s::foo() called with %p\n", __FILE__, f->name, args);
+    return NULL;
+}
+
+static Var *
+bar(vfuncptr f, Var *args)
+{
+    parse_error("%s:%s::bar() called with %p\n", __FILE__, f->name, args);
+    return NULL;
+}
+
+DV_MOD_EXPORT void
+dv_module_fini(
+    const char *name
+)
+{
+	parse_error("*******************************************************");
+	parse_error("********** mod1.c finalized                ************");
+	parse_error("*******************************************************");
+}
+
