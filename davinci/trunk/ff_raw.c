9ce074f5a325508e20c6ee93104602a9501013a1
gorelick <gorelick@dedd6328-c50f-0410-8be5-b54a49638459>
2002-03-22 23:07:27 +0000

    Rewrote to take x,y,z,org,format

--- davinci/trunk/ff_raw.c
+++ davinci/trunk/ff_raw.c
@@ -1,4 +1,5 @@
 #include "parser.h"
+#include "iomedley.h"
 
 
 Var *
@@ -6,58 +7,110 @@ ff_raw(vfuncptr func, Var * arg)
 {
 	unsigned char *buf;
 	char *filename=NULL;
-	int Row=0;
-	int Col=0;
-	int size;
 	FILE *fp;
-	int type;
-	int ac;
-	Var **av;
-	int unit=1;
-	Alist alist[5];
-	alist[0] = make_alist( "filename",  ID_STRING,  NULL,     &filename);
-	alist[1] = make_alist( "row", INT,NULL,&Row);
-	alist[2] = make_alist( "col", INT,NULL,&Col);
-	alist[3] = make_alist( "unit", INT,NULL,&unit);
-	alist[4].name = NULL;
+	int x = -1,y = -1,z =-1,header;
+	char *org = NULL;
+	char *format =NULL;
+	struct iom_iheader h;
+	char *data;
+	Var *v;
 
+	char *orgs[] = { "bil", "bip", "bsq" };
+	char *formats[] = { 
+		"byte", "msb_short", "msb_int", "msb_float", "msb_double" ,
+		        "lsb_short", "lsb_int", "lsb_float", "lsb_double" ,
+		};
+
+	Alist alist[11];
+	alist[0] = make_alist( "filename",  ID_STRING,  NULL,     &filename);
+	alist[1] = make_alist( "x", INT,NULL,&x);
+	alist[2] = make_alist( "y", INT,NULL,&y);
+	alist[3] = make_alist( "z", INT,NULL,&z);
+	alist[4] = make_alist( "org", ID_ENUM,orgs,&org);
+	alist[5] = make_alist( "format", ID_ENUM,formats,&format);
+	alist[6] = make_alist( "header", INT,NULL,&header);
+	alist[7].name = NULL;
 
 	if (parse_args(func, arg, alist) == 0) return(NULL);
 
-        if (filename == NULL) {
-        	parse_error("No filename specified to load()");
-        	return (NULL);
-        }
+	if (filename == NULL) {
+		parse_error("No filename specified to load()");
+		return (NULL);
+	}
+	if (x == -1 ||  y == -1 || z ==-1 || org == NULL || format==NULL) {
+		parse_error("usage: %s(filename=,x=,y=,z=,org=,format=,[header=])", 
+			func->name);
+		return(NULL);
+	}
 
 	if ((fp=fopen(filename,"r"))==NULL){
 		parse_error("Can't find file: %s",filename);
 		return(NULL);
 	}
 
-	if (!(Row) || !(Col)) {
-		parse_error("Must specify row and column size!");
+	iom_init_iheader(&h);
+
+	if (!strcmp(format, "byte")) {
+		h.eformat = iom_MSB_INT_1;
+		h.format = iom_BYTE;
+	} else if (!strcmp(format, "msb_short")) {
+		h.eformat = iom_MSB_INT_2;
+		h.format = iom_SHORT;
+	} else if (!strcmp(format, "lsb_short")) {
+		h.eformat = iom_LSB_INT_2;
+		h.format = iom_SHORT;
+	} else if (!strcmp(format, "msb_int")) {
+		h.eformat = iom_MSB_INT_4;
+		h.format = iom_INT;
+	} else if (!strcmp(format, "lsb_int")) {
+		h.eformat = iom_LSB_INT_4;
+		h.format = iom_INT;
+	} else if (!strcmp(format, "msb_float")) {
+		h.eformat = iom_MSB_IEEE_REAL_4;
+		h.format = iom_FLOAT;
+	} else if (!strcmp(format, "lsb_float")) {
+		h.eformat = iom_LSB_IEEE_REAL_4;
+		h.format = iom_FLOAT;
+	} else if (!strcmp(format, "msb_double")) {
+		h.eformat = iom_MSB_IEEE_REAL_8;
+		h.format = iom_DOUBLE;
+	} else if (!strcmp(format, "lsb_double")) {
+		h.eformat = iom_LSB_IEEE_REAL_8;
+		h.format = iom_DOUBLE;
+	} else {
+		parse_error("Unrecognized format: %s\n", format);
 		return(NULL);
 	}
 
-	buf=(unsigned char *)calloc((Row*Col),unit);
-
-	size=fread(buf,unit,(Row*Col),fp);
-	if (size < (Row*Col)){
-		parse_error("Incorrect Row/Col size, sorry...aborting");
+	if (!strcmp(org, "bsq")) {
+		h.size[0] = x;
+		h.size[1] = y;
+		h.size[2] = z;
+		h.org = iom_BSQ;
+	} else if (!strcmp(org, "bil")) {
+		h.size[0] = x;
+		h.size[1] = z;
+		h.size[2] = y;
+		h.org = iom_BIL;
+	} else if (!strcmp(org, "bip")) {
+		h.size[0] = z;
+		h.size[1] = x;
+		h.size[2] = y;
+		h.org = iom_BIL;
+	} else {
+		parse_error("Unrecognized org: %s\n", org);
 		return(NULL);
 	}
 
-	if (unit == 1)
-		type=BYTE;
-	else if (unit == 2)
-		type = SHORT;
-	else if (unit == 4)
-		type = INT; //or could be float??
-	else if (unit == 8)
-		type = DOUBLE;
-	else
-		type=BYTE;	
-	
-	return(newVal(BSQ,Col,Row,1,type,buf));
+	h.dptr = header;
+	h.gain = 1.0;
+	h.offset = 0.0;
+	h.prefix[0] = h.prefix[1] = h.prefix[2] = 0;
+	h.suffix[0] = h.suffix[1] = h.suffix[2] = 0;
+
+	data = iom_read_qube_data(fileno(fp), &h);
+	v = iom_iheader2var(&h);
+	V_DATA(v)  = data;
+
+	return(v);
 }
-
