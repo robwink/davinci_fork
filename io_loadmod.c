d154de29a4ee92d94b79b25a8d0cbc15b6a8afc8
randy <randy@dedd6328-c50f-0410-8be5-b54a49638459>
2002-04-17 04:34:02 +0000

    Support code for Read/Write loadable module extensions.  This is still in development... this code doesn't do much of anything yet, and isn't explicitly included in the Makefile yet.

---
+++ io_loadmod.c
@@ -0,0 +1,183 @@
+#include "io_loadmod.h"
+#ifdef USE_HPUX_SHL
+#include <dl.h>
+#elif defined(_WIN32)
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
+
+IOmodPtr IOmodList = NULL;
+IOmodPtr IOmodHeap = NULL;
+
+static void portable_dlclose(MODHANDLE closeme) {
+  /* a simple cross platform dlclose routine */
+#ifdef USE_HPUX_SHL
+  shl_unload(closeme);
+#elif defined(_WIN32)
+  FreeLibrary(closeme);
+#else
+  dlclose(closeme);
+#endif 
+}
+
+static int duplicate_name(char * check) {
+  /* verifies that a module with this name is not already in the heap */
+  IOmodPtr current;
+  current = IOmodHeap;
+  while (current) {
+    if (!strcmp(check, current->modname)) return 1;
+    current = current->next_heap;
+  }
+  return 0;
+}
+
+static void activate_module(IOmodPtr newmod) {
+  if (newmod->prev_list != NULL) {
+    parse_error("Module %s is already active.\n", newmod->modname);
+    return;
+  }
+  if (IOmodList == NULL) { /* first active module */
+    IOmodList = newmod;
+    IOmodList->prev_list = newmod;
+  }
+  newmod->prev_list = IOmodList->prev_list;
+  IOmodList->prev_list = newmod;
+  newmod->prev_list->next_list = newmod;
+  newmod->next_list = NULL; /* In case this is the first node */
+  return;
+}
+
+static void deactivate_module(IOmodPtr killme) {
+  if (killme->prev_list == NULL) {
+    parse_error("Module %s is already inactive.\n", killme->modname);
+  }
+  if (killme == IOmodList) {
+    IOmodList = killme->next_list;
+  }
+  killme->next_list->prev_list = killme->prev_list;
+  if (killme->prev_list->next_list != NULL) 
+    killme->prev_list->next_list = killme->next_list;
+  killme->prev_list = NULL;
+  killme->next_list = NULL;
+  return;
+}
+
+static MODHANDLE portable_dlopen(char * fname) {
+  /* a simple Cross platform dlopen routine */
+  MODHANDLE rtn;
+#ifdef USE_HPUX_SHL
+  rtn = shl_load(fname, BIND_DEFERRED, BIND_VERBOSE);
+  if (rtn == NULL) {
+    parse_error("Error opening shared lib %s: %s\n", fname, strerror(errno));
+  }
+#elif defined(_WIN32)
+  rtn = LoadLibrary(fname);
+  if (rtn == NULL) {
+    parse_error("Error opening DLL %s: Get a real operating system.\n");
+  }
+#else
+  rtn = dlopen(fname, RTLD_LAZY);
+  if (rtn == NULL) {
+    parse_error("Error opening shared lib %s: %s\n", fname, dlerror());
+  }
+#endif
+}
+
+static void * portable_dlsym(MODHANDLE solib, char * function) {
+  /* a simple cross platform dlsym routine */
+  void * rtnfunc = NULL;
+#ifdef USE_HPUX_SHL
+  if (shl_findsym(solib, function, TYPE_PROCEDURE, &rtnfunc) < 0)
+    rtnfunc = NULL;
+#elif defined(_WIN32)
+  rtnfunc = (WIN_DLL_IMPORT)GetProcAddress(solib, function);
+#else
+  rtnfunc = dlsym(solib, function);
+#endif
+}
+
+IOmodPtr new_io_module() {
+  /* return an allocated IOmod structure */
+  IOmodPtr new_mod;
+  if ((new_mod = (IOmodPtr)malloc(sizeof(IOmod))) == NULL) {
+    parse_error("Cannot alloc a new IOmod struct.\n");
+    return NULL;
+  }
+  /* initialize the struct */
+  new_mod->modname = NULL;
+  new_mod->dlhandle = NULL;
+  new_mod->implement = 0;
+  new_mod->read_func = NULL;
+  new_mod->write_func = NULL;
+  new_mod->next_list = NULL;
+  new_mod->prev_list = NULL;
+  if (IOmodHeap == NULL) {
+    IOmodHeap = new_mod;
+    new_mod->prev_heap = new_mod;
+  }
+  else {
+    IOmodHeap->prev_heap->next_heap = new_mod;
+    IOmodHeap->prev_heap = new_mod;
+  }
+  new_mod->next_heap = NULL;
+  return new_mod;
+}
+
+void destroy_io_module(IOmodPtr killme) {
+  if (killme == IOmodHeap) {
+    IOmodHeap = killme->next_heap;
+  }
+  if (killme == IOmodList) {
+    IOmodList = killme->next_list;
+  }
+  if (killme->prev_heap != NULL)
+    killme->prev_heap->next_heap = killme->next_heap;
+  if (killme->next_heap != NULL) 
+    killme->next_heap->prev_heap = killme->prev_heap;
+  if (killme->prev_list != NULL)
+    killme->prev_list->next_list = killme->next_list;
+  if (killme->next_list != NULL) 
+    killme->next_list->prev_list = killme->prev_list;
+
+  if (killme->dlhandle != NULL) {
+    portable_dlclose(killme->dlhandle);
+    killme->dlhandle = NULL;
+  }
+  if (killme->modname != NULL) {
+    free(killme->modname);
+    killme->modname = NULL;
+  }
+  free(killme);
+  return;
+} 
+
+IOmodPtr open_io_module(char * modname, char * modpath) {
+  /* Open an IO module specified in the mod argument.
+     if successful, returns a new IOmod structure, NULL if it fails.
+  */
+  IOmodPtr new_module = NULL;
+  MODHANDLE candidate;
+
+  candidate = portable_dlopen(modpath);
+  if (candidate == NULL) goto error_exit;
+  new_module = new_io_module();
+  new_module->dlhandle = candidate;
+  if ((new_module->modname = malloc(strlen(modname)+1)) == NULL) {
+    parse_error("Can't malloc a string for IO module.\n");
+    goto error_exit;
+  }
+  if (duplicate_name(modname)) {
+    parse_error("Duplicate module name '%s'.\n", modname);
+    goto error_exit;
+  }
+
+  return new_module;
+ error_exit:
+  /* destroy_io_module will handle the cleanup of any sort of
+     shared library opens or memeory allocs we do in this routine 
+  */
+  if (new_module != NULL) destroy_io_module(new_module);
+  return NULL;
+}
+  
