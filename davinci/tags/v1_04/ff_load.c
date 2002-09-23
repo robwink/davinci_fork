d085e8b9e972e8ef11fc6c04c09e064eac3732f6
nobody <nobody@dedd6328-c50f-0410-8be5-b54a49638459>
2002-09-23 19:00:37 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_04'.

---
+++ davinci/tags/v1_04/ff_load.c
@@ -0,0 +1,130 @@
+#include "parser.h"
+#include "iomedley.h"
+#include "dvio.h"
+#include "dvio_specpr.h"
+
+
+Var *
+ff_load_many(vfuncptr func, Var * arg)
+{
+	int i;
+	char *filename;
+	Var *s, *t;
+	if (arg == NULL || V_TYPE(arg) != ID_TEXT) {
+        parse_error("No filenames specified to %s()", func->name);
+        return (NULL);
+	}
+
+	s = new_struct(V_TEXT(arg).Row);
+	for (i = 0 ; i < V_TEXT(arg).Row ; i++) {
+		filename = strdup(V_TEXT(arg).text[i]);
+		t = ff_load(func, newString(filename));
+		if (t) add_struct(s, filename, t);
+	}
+	if (get_struct_count(s)) {
+		return(s);
+	}  else {
+		return(NULL);
+	}
+}
+
+
+Var *
+ff_load(vfuncptr func, Var * arg)
+{
+    int record = -1;
+    FILE *fp = NULL;
+    Var  *input = NULL;
+    char *filename = NULL;
+    char *p, *fname;
+	struct iom_iheader h;
+	Var *fvar = NULL;
+
+	Alist alist[12];
+	alist[0] = make_alist( "filename",  ID_UNK,  NULL,     &fvar);
+	alist[1] = make_alist( "record",    INT,    	NULL,     &record);
+	alist[2] = make_alist( "xlow",      INT,    	NULL,     &h.s_lo[0]);
+	alist[3] = make_alist( "xhigh",     INT,    	NULL,     &h.s_hi[0]);
+	alist[4] = make_alist( "xskip",     INT,    	NULL,     &h.s_skip[0]);
+	alist[5] = make_alist( "ylow",      INT,    	NULL,     &h.s_lo[1]);
+	alist[6] = make_alist( "yhigh",     INT,    	NULL,     &h.s_hi[1]);
+	alist[7] = make_alist( "yskip",     INT,    	NULL,     &h.s_skip[1]);
+	alist[8] = make_alist( "zlow",      INT,    	NULL,     &h.s_lo[2]);
+	alist[9] = make_alist( "zhigh",     INT,    	NULL,     &h.s_hi[2]);
+	alist[10] = make_alist( "zskip",     INT,    	NULL,     &h.s_skip[2]);
+	alist[11].name = NULL;
+
+	if (parse_args(func, arg, alist) == 0) return(NULL);
+
+	if (fvar == NULL) {
+        parse_error("No filename specified to load()");
+        return (NULL);
+	}
+	if (V_TYPE(fvar) == ID_TEXT) {
+		return(ff_load_many(func, fvar));
+	} else if (V_TYPE(fvar) == ID_STRING) {
+		filename = V_STRING(fvar);
+	} else {
+        parse_error("Illegal argument to function %s(%s), expected STRING", 
+			func->name, "filename");
+        return (NULL);
+	}
+
+    /** 
+     ** if open file fails, check for record suffix
+     **/
+    if ((fname = dv_locate_file(filename)) == NULL) {
+        if ((p = strchr(filename, SPECPR_SUFFIX)) != NULL) {
+            *p = '\0';
+            record = atoi(p + 1);
+            fname = dv_locate_file(filename);
+        }
+        if (fname == NULL) {
+            sprintf(error_buf, "Cannot find file: %s", filename);
+            parse_error(NULL);
+            return (NULL);
+        }
+    }
+
+	iom_init_iheader(&h);
+	if (record != -1) h.s_lo[2] = h.s_hi[2] = record;
+
+    if (fname && (fp = fopen(fname, "rb")) != NULL) {
+        if (iom_is_compressed(fp))
+            fp = iom_uncompress(fp, fname);
+
+        if (input == NULL)    input = LoadSpecpr(fp, fname, record);
+        if (input == NULL)    input = dv_LoadVicar(fp, fname, &h);
+        if (input == NULL)    input = dv_LoadISIS(fp, fname, &h);
+        if (input == NULL)    input = dv_LoadGRD(fp, fname, &h);
+        if (input == NULL)    input = dv_LoadPNM(fp, fname, &h);
+        if (input == NULL)    input = dv_LoadIMath(fp, fname, &h);
+        if (input == NULL)    input = dv_LoadGOES(fp, fname, &h);
+        if (input == NULL)    input = dv_LoadAVIRIS(fp, fname, &h);
+        if (input == NULL)    input = dv_LoadENVI(fp, fname, &h);
+
+#ifdef HAVE_LIBHDF5
+        if (input == NULL)    input = LoadHDF5(fname);
+#endif
+#ifdef BUILD_MODULE_SUPPORT
+	if (input == NULL)    input = read_from_io_module(fp);
+#endif
+		/* Libmagic should always be the last chance */
+#ifdef HAVE_LIBMAGICK
+        if (input == NULL)
+	         input = dvReadImage(func, arg); // last gasp attempt.
+#endif /* HAVE_LIBMAGICK */
+
+        fclose(fp);
+		
+
+        if (input == NULL) {
+            sprintf(error_buf, "Unable to determine file type: %s", filename);
+            parse_error(NULL);
+        }
+    }
+    if (fname)
+        free(fname);
+    
+    return (input);
+}
---
+++ davinci/tags/v1_04/ff_raw.c
@@ -0,0 +1,116 @@
+#include "parser.h"
+#include "iomedley.h"
+
+
+Var *
+ff_raw(vfuncptr func, Var * arg)
+{
+	unsigned char *buf;
+	char *filename=NULL;
+	FILE *fp;
+	int x = -1,y = -1,z =-1,header;
+	char *org = NULL;
+	char *format =NULL;
+	struct iom_iheader h;
+	char *data;
+	Var *v;
+
+	char *orgs[] = { "bil", "bip", "bsq" };
+	char *formats[] = { 
+		"byte", "msb_short", "msb_int", "msb_float", "msb_double" ,
+		        "lsb_short", "lsb_int", "lsb_float", "lsb_double" ,
+		};
+
+	Alist alist[11];
+	alist[0] = make_alist( "filename",  ID_STRING,  NULL,     &filename);
+	alist[1] = make_alist( "x", INT,NULL,&x);
+	alist[2] = make_alist( "y", INT,NULL,&y);
+	alist[3] = make_alist( "z", INT,NULL,&z);
+	alist[4] = make_alist( "org", ID_ENUM,orgs,&org);
+	alist[5] = make_alist( "format", ID_ENUM,formats,&format);
+	alist[6] = make_alist( "header", INT,NULL,&header);
+	alist[7].name = NULL;
+
+	if (parse_args(func, arg, alist) == 0) return(NULL);
+
+	if (filename == NULL) {
+		parse_error("No filename specified to load()");
+		return (NULL);
+	}
+	if (x == -1 ||  y == -1 || z ==-1 || org == NULL || format==NULL) {
+		parse_error("usage: %s(filename=,x=,y=,z=,org=,format=,[header=])", 
+			func->name);
+		return(NULL);
+	}
+
+	if ((fp=fopen(filename,"rb"))==NULL){
+		parse_error("Can't find file: %s",filename);
+		return(NULL);
+	}
+
+	iom_init_iheader(&h);
+
+	if (!strcmp(format, "byte")) {
+		h.eformat = iom_MSB_INT_1;
+		h.format = iom_BYTE;
+	} else if (!strcmp(format, "msb_short")) {
+		h.eformat = iom_MSB_INT_2;
+		h.format = iom_SHORT;
+	} else if (!strcmp(format, "lsb_short")) {
+		h.eformat = iom_LSB_INT_2;
+		h.format = iom_SHORT;
+	} else if (!strcmp(format, "msb_int")) {
+		h.eformat = iom_MSB_INT_4;
+		h.format = iom_INT;
+	} else if (!strcmp(format, "lsb_int")) {
+		h.eformat = iom_LSB_INT_4;
+		h.format = iom_INT;
+	} else if (!strcmp(format, "msb_float")) {
+		h.eformat = iom_MSB_IEEE_REAL_4;
+		h.format = iom_FLOAT;
+	} else if (!strcmp(format, "lsb_float")) {
+		h.eformat = iom_LSB_IEEE_REAL_4;
+		h.format = iom_FLOAT;
+	} else if (!strcmp(format, "msb_double")) {
+		h.eformat = iom_MSB_IEEE_REAL_8;
+		h.format = iom_DOUBLE;
+	} else if (!strcmp(format, "lsb_double")) {
+		h.eformat = iom_LSB_IEEE_REAL_8;
+		h.format = iom_DOUBLE;
+	} else {
+		parse_error("Unrecognized format: %s\n", format);
+		return(NULL);
+	}
+
+	if (!strcmp(org, "bsq")) {
+		h.size[0] = x;
+		h.size[1] = y;
+		h.size[2] = z;
+		h.org = iom_BSQ;
+	} else if (!strcmp(org, "bil")) {
+		h.size[0] = x;
+		h.size[1] = z;
+		h.size[2] = y;
+		h.org = iom_BIL;
+	} else if (!strcmp(org, "bip")) {
+		h.size[0] = z;
+		h.size[1] = x;
+		h.size[2] = y;
+		h.org = iom_BIL;
+	} else {
+		parse_error("Unrecognized org: %s\n", org);
+		return(NULL);
+	}
+
+	h.dptr = header;
+	h.gain = 1.0;
+	h.offset = 0.0;
+	h.prefix[0] = h.prefix[1] = h.prefix[2] = 0;
+	h.suffix[0] = h.suffix[1] = h.suffix[2] = 0;
+
+	data = iom_read_qube_data(fileno(fp), &h);
+	v = iom_iheader2var(&h);
+	V_DATA(v)  = data;
+
+	return(v);
+}
