d1a8b577187060da69efe1f46ff3a96ec8eba4c4
gorelick <gorelick@dedd6328-c50f-0410-8be5-b54a49638459>
2003-02-18 19:40:28 +0000

    Moved to parse_args

--- davinci/trunk/ff_rgb.c
+++ davinci/trunk/ff_rgb.c
@@ -11,51 +11,41 @@ Var *
 ff_rgb(vfuncptr func, Var *arg)
 {
     int count, i;
-    Var *v[3], *s;
+    Var *v[3] = { NULL, NULL, NULL };
+	Var *s;
     int x,y;
     u_char *data;
 
-    struct keywords kw[] = {
-        { "red", NULL },
-        { "green", NULL },
-        { "blue", NULL },
-        { NULL, NULL }
-    };
+	Alist alist[4];
+	alist[0] = make_alist( "red",    ID_VAL,    NULL,    &v[0]);
+	alist[1] = make_alist( "green",  ID_VAL,    NULL,    &v[1]);
+	alist[2] = make_alist( "blue",   ID_VAL,    NULL,    &v[2]);
+	alist[3].name = NULL;
 
-    if (evaluate_keywords(func, arg, kw)) {
-        return(NULL);
-    }
-
-    v[0] = get_kw("red", kw);
-    v[1] = get_kw("green", kw);
-    v[2] = get_kw("blue", kw);
+	if (parse_args(func, arg, alist) == 0) return(NULL);
 
     for (i = 0 ; i < 3 ; i++) {
-        if ((s = eval(v[i])) != NULL) {
-            v[i] = s;
-        }
+		if (v[i] == NULL) {
+			parse_error("rgb() requires three images.  Image %d is NULL", i+1);
+			return(NULL);
+		}
         if (V_TYPE(v[i]) != ID_VAL || V_FORMAT(v[i]) != BYTE) {
-            sprintf(error_buf, 
-                    "%s: Image planes must be BYTE format\n",func->name);
-            parse_error(NULL);
+            parse_error("%s: Image planes must be BYTE format",func->name);
             return(NULL);
         }
         if (V_ORG(v[i]) != V_ORG(v[0]))  {
-            sprintf(error_buf, 
-                    "%s: Image planes must have the same org\n",func->name);
-            parse_error(NULL);
+            parse_error( 
+                    "%s: Image planes must have the same org",func->name);
             return(NULL);
         }
         if (V_SIZE(v[i])[0] != V_SIZE(v[0])[0]) {
-            sprintf(error_buf, 
-                    "%s: Image planes must be the same size\n",func->name);
-            parse_error(NULL);
+            parse_error( 
+                    "%s: Image planes must be the same size",func->name);
             return(NULL);
         }
         if (GetBands(V_SIZE(v[i]), V_ORG(v[i])) != 1) {
-            sprintf(error_buf, 
-                    "%s: Image planes must have depth=1\n",func->name);
-            parse_error(NULL);
+            parse_error( 
+                    "%s: Image planes must have depth=1",func->name);
             return(NULL);
         }
     }
