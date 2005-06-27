66f8021a2ae5acf8845bf18fe009556c471b16f7
eengle <eengle@dedd6328-c50f-0410-8be5-b54a49638459>
2005-06-27 23:09:57 +0000

    Added combobox widget support to gui module

---
+++ modules/widget_combobox.h
@@ -0,0 +1,30 @@
+/* widget_text.h
+ *
+ * Davinci GUI binding for various Motif widgets.
+ *
+ * Copyright 2005
+ * Mars Space Flight Facility
+ * Department of Geological Sciences
+ * Arizona State University
+ *
+ * Eric Engle <eric.engle@asu.edu>
+ *
+ */
+
+#ifndef DV_WIDGET_COMBOBOX_H
+#define DV_WIDGET_COMBOBOX_H
+
+#include <Xm/ComboBox.h>
+#include "gui.h"
+
+/*****************************************************************************
+ *
+ * FUNCTION PROTOTYPES
+ *
+ *****************************************************************************/
+
+int				gui_isComboBox(const char *);
+WidgetClass		gui_getComboBoxClass(void);
+CallbackList	gui_getComboBoxCallbacks(void);
+
+#endif /* DV_WIDGET_COMBOBOX_H */
