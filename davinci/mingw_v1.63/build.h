0f3c9d32789c586196445fc0780032947cfae314
bdeva <bdeva@dedd6328-c50f-0410-8be5-b54a49638459>
2007-02-28 00:13:54 +0000

    directory mistake

--- davinci/mingw_v1.63/build.h
+++
@@ -1,2 +0,0 @@
-char *build = "Mon Sep 18 12:44:06 MST 2006";
-char *builder = "gorelick@daedalia";
--- davinci/mingw_v1.63/endian_norm.h
+++
@@ -1,7 +0,0 @@
-#include "parser.h"
-#define intswap(i) (i) = ((((i) >> 24) & 0xFF) | (((i) >> 8) & 0xFF00) | \
-	       (((i) << 8) &0xFF0000) | (((i) << 24) & 0xFF000000))
-char * flip_endian(unsigned char *, unsigned int, unsigned int);
-char * var_endian(Var *);
-
-
