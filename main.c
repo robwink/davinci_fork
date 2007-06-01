a11e41e026a0173f0b27fb64e696e35cb6e25b1b
bdeva <bdeva@dedd6328-c50f-0410-8be5-b54a49638459>
2007-06-01 17:01:35 +0000

    Some functions were moved from here to globals.c

--- main.c
+++ main.c
@@ -21,22 +21,22 @@ XtAppContext    applicationContext;
 #define ctime_r(n, m) ctime(n)
 #endif
 
-int interactive = 1;
-int continuation = 0;
-int in_comment = 0;
-int in_string = 0;
+extern int interactive;
+extern int continuation;
+extern int in_comment;
+extern int in_string;
 
-int debug = 0;
-char pp_input_buf[8192];
-FILE *lfile = NULL;
-FILE *pfp = NULL;
+extern int debug;
+extern char pp_input_buf[8192];
+extern FILE *lfile;
+extern FILE *pfp;
 
-int SCALE = 6;
-int VERBOSE = 2;
-int DEPTH = 2;
+extern int SCALE;
+extern int VERBOSE;
+extern int DEPTH ;
 
-int allocs = 0;
-Var *VZERO;
+extern int allocs;
+extern Var *VZERO;
 
 static int windows = 1;
 int usage(char *prog);
@@ -44,9 +44,9 @@ int usage(char *prog);
 /**
  ** This is stuff from the old input.c
  **/
-int pp_count = 0;
-int pp_line = 0;
-int indent = 0;
+extern pp_count;
+extern pp_line;
+extern indent;
 
 /**
  ** Command line args:
@@ -540,7 +540,7 @@ process_streams(void)
     }
 }
 
-Var *curnode = NULL;
+extern Var *curnode;
 
 void
 parse_buffer(char *buf)
@@ -581,64 +581,6 @@ parse_buffer(char *buf)
     if (parent_buffer) yy_switch_to_buffer(parent_buffer);
 }
 
-/*
-** This is similar to parse_buffer, but it doesn't print the stack
-** or clean up the scope
-*/
-Var *
-eval_buffer(char *buf)
-{
-    int i,j;
-    extern char *yytext;
-    Var *v = NULL;
-    void *parent_buffer;
-    void *buffer;
-    Var *node;
-
-    parent_buffer = (void *) get_current_buffer();
-    buffer = (void *) yy_scan_string(buf);
-
-    while((i = yylex()) != 0) {
-        /*
-        ** if this is a function definition, do no further parsing yet.
-        */
-        j = yyparse(i, (Var *)yytext);
-        if (j == -1) quit();
-
-        if (j == 1 && curnode != NULL) {
-            node = curnode;
-            evaluate(node);
-            /* // v = pop(scope_tos()); */
-            free_tree(node);
-        }
-    }
-
-    yy_delete_buffer((struct yy_buffer_state *)buffer);
-    if (parent_buffer) yy_switch_to_buffer(parent_buffer);
-    return(v);
-}
-
-void
-quit(void)
-{
-    char cmd[256];
-    char *path = getenv("TMPDIR");
-
-    if (interactive) {
-        printf("\n");
-#if defined(USE_X11_EVENTS) && defined(HAVE_LIBREADLINE)
-    /* JAS FIX */
-        rl_callback_handler_remove();
-#endif
-    }
-
-    /**
-    ** clean up temporary directory
-    **/
-    rmrf(path);
-    exit(0);
-}
-
 void
 log_line(char *str)
 {
@@ -648,21 +590,6 @@ log_line(char *str)
     }
 }
 
-void 
-yyerror(char *s)
-{
-    extern int pp_count;
-    extern int pp_line;
-
-    printf("***%*s^ ", pp_count, " ");
-    printf("%s, line %d\n", s, pp_line);
-}
-
-int 
-yywrap()
-{
-    return (1);
-}
 
 void
 fake_data()
@@ -735,34 +662,6 @@ fake_data()
 
 }
 
-void 
-make_sym(Var * v, int format, char *str)
-{
-    V_FORMAT(v) = format;
-    V_DSIZE(v) = 1;
-    V_SIZE(v)[0] = V_SIZE(v)[1] = V_SIZE(v)[2] = 1;
-    V_ORG(v) = BSQ;
-    V_DATA(v) = calloc(1, NBYTES(format));
-
-    switch (format) {
-    case INT:
-        *((int *) (V_DATA(v))) = strtol(str, NULL, 10);
-        break;
-    case FLOAT:{
-        double d;
-        d = strtod(str, NULL);
-        if (d > MAXFLOAT || d < MINFLOAT && d != 0) {
-            free(V_DATA(v));
-            V_DATA(v) = calloc(1, NBYTES(DOUBLE));
-            V_FORMAT(v) = DOUBLE;
-            *((double *) (V_DATA(v))) = d;
-        } else {
-            *((float *) (V_DATA(v))) = d;
-        }
-    }
-    }
-}
-
 void
 env_vars()
 {
@@ -776,62 +675,6 @@ env_vars()
         V_STRING(s) = strdup(path);
         put_sym(s);
     }
-}
-
-char *
-unquote(char *name)
-{
-    char *p = name;
-
-    if (name == NULL) return(NULL);
-    if (*name == 0) return(name);
-    if (*name == '"') {
-        p++;
-        name++;
-        while (*name) {
-            if (*name == '"' && *(name - 1) != '\\')
-                break;
-            name++;
-        }
-        *name = '\0';
-    } else if (*name == '\'') {
-        p++;
-        name++;
-        while (*name) {
-            if (*name == '\'' && *(name - 1) != '\\')
-                break;
-            name++;
-        }
-        *name = '\0';
-    }
-    return (p);
-}
-
-char *
-unescape(char *str)
-{
-    char *p = str;
-    char *q = str;
-
-    if (str && *str) {
-        while (*p) {
-            if (*p == '\\') {
-                if (*(p + 1) == 't')
-                    *q = '\t';
-                else if (*(p + 1) == 'n')
-                    *q = '\n';
-                else
-                    *q = *(p + 1);
-                p++;
-            } else {
-                *q = *p;
-            }
-            p++;
-            q++;
-        }
-        *q = *p;
-    }
-    return (str);
 }
 
 void
