dd8dc42e741fb6126a2560533cae35fd79639308
cedwards <cedwards@dedd6328-c50f-0410-8be5-b54a49638459>
2008-01-10 06:02:09 +0000

    modified to do an automatic function lenght list

--- modules/cse/mod_cse.c
+++ modules/cse/mod_cse.c
@@ -57,7 +57,7 @@ static dvModuleFuncDesc exported_list[] = {
 };
 
 static dvModuleInitStuff is = {
-  exported_list, 20,
+  exported_list, sizeof(exported_list)/sizeof(dvModuleFuncDesc),,
   NULL, 0
 };
 
