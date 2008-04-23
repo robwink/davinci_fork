987f7d4eb37f7933eff3f71ace10abb6af87ad06
bdeva <bdeva@dedd6328-c50f-0410-8be5-b54a49638459>
2008-04-23 21:48:53 +0000

    This is encloses version 1.72 * Fixed the image_resize downsampling method (fixed ignore value) * Load vanilla input now has filename expansion (i.e. can use ~/user) + Added remote file loading functionality (using libcurl).   Protocols supported: HTTP, HTTPS, FTP, SFTP, FILE.   Involved core dvio change

---
+++ davinci/trunk/url_create_file.c
@@ -0,0 +1,131 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+
+#ifdef HAVE_LIBCURL
+#include <curl/curl.h>
+#endif
+
+#include <string.h>
+
+//These are also located in dvio_pds.c
+#define HTTP_PREFIX  "http://"
+#define HTTPS_PREFIX  "https://"
+#define FTP_PREFIX "ftp://"
+#define SFTP_PREFIX "sftp://"
+#define FILE_PREFIX "file:///"
+
+
+
+
+FILE *url_file; /* declare a FILE pointer */
+int unsigned long  url_data_counter = 0;
+
+size_t	url_callback(char *, size_t , size_t, void *);
+
+
+/*
+ * try_remote_load - will identify if the filename is remote, 
+ * and loads it if it has been downloaded properly. The filename
+ * changes
+ */
+char * try_remote_load(char * filename){
+	char * tmpfilename;
+      
+#ifdef HAVE_LIBCURL	
+
+	if(filename != NULL){
+	   //checks if URL
+		if(	(strncasecmp(filename, FILE_PREFIX, strlen(FILE_PREFIX)) == 0) ||
+			(strncasecmp(filename, HTTP_PREFIX, strlen(HTTP_PREFIX)) == 0) ||
+			(strncasecmp(filename, HTTPS_PREFIX, strlen(HTTPS_PREFIX)) == 0) ||
+			(strncasecmp(filename, FTP_PREFIX, strlen(FTP_PREFIX)) == 0) ||
+			(strncasecmp(filename, SFTP_PREFIX, strlen(SFTP_PREFIX)) == 0)){
+				tmpfilename = (char *)make_temp_file_path();
+ 				//Now filename means URL
+				if(tmpfilename != NULL  && url_create_file(tmpfilename, filename) == 0){
+				   strcpy(filename, tmpfilename);
+				}				
+
+				if(tmpfilename != NULL){
+					free(tmpfilename);
+				}
+	   	}
+	}  
+
+#endif
+	 return filename;   
+}
+
+#ifdef HAVE_LIBCURL	
+int  url_create_file(char * filename, char * url){
+	CURL *curl;
+	CURLcode res;
+	char errorbuff[CURL_ERROR_SIZE];
+	curl = curl_easy_init();
+	      
+	if(curl) {
+		//Open file for writing
+		url_file = fopen(filename, "wb"); 
+		if(url_file==NULL) {
+			parse_error("Error: can't open file for writing.\n");
+			return 1;
+		}
+
+//		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+		curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
+		curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorbuff);
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "davinci-curl");
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, url_callback);
+
+		parse_error2("Downloading.");
+		res = curl_easy_perform(curl);
+
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+		curl_global_cleanup();
+
+		fclose(url_file);
+		url_file = NULL;
+
+		if(res != CURLE_OK ){
+		   parse_error("Download error: %s", errorbuff);
+		   return 1;
+		}else{
+			parse_error2("done.\n");
+		}
+	}else{
+	   return 1;
+	}
+	return 0;
+}
+
+
+
+size_t	url_callback(char *buffer, size_t size, size_t nmemb, void *data)
+{
+	size_t realsize = size * nmemb;
+	char c;
+	int i=0;
+	while(i < realsize) {     /* keep looping... */
+		c =  buffer[i];
+		fputc(c, url_file);  
+		i++;
+	}
+
+	url_data_counter += realsize;
+	//Show a dot every 200KB
+	if(url_data_counter  >= 200000 ){
+	   parse_error2(".");
+	   url_data_counter = 0;
+	}
+	
+
+	return realsize;
+}
+
+#endif
