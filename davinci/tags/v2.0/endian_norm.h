7bb733c826061ba69606acff88fe663beca037b4
randy <randy@dedd6328-c50f-0410-8be5-b54a49638459>
2009-06-24 22:52:41 +0000

    Untag broken v2.0 thm module

--- davinci/tags/v2.0/endian_norm.h
+++
@@ -1,8 +0,0 @@
-#include "parser.h"
-#define intswap(i) (i) = ((((i) >> 24) & 0xFF) | (((i) >> 8) & 0xFF00) | \
-	       (((i) << 8) &0xFF0000) | (((i) << 24) & 0xFF000000))
-char * flip_endian(unsigned char *, unsigned int, unsigned int);
-char * var_endian(Var *);
-
-void MSB(unsigned char * data, unsigned int data_elem, unsigned int word_size);
-void LSB(unsigned char * data, unsigned int data_elem, unsigned int word_size);
