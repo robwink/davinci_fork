7fef76dd55d30bd95d6f9e87b15d1d9890fdfa17
gorelick <gorelick@dedd6328-c50f-0410-8be5-b54a49638459>
2009-08-04 08:52:31 +0000

    Deleting branch

--- davinci/branches/gorelick-20090804/endian_norm.h
+++
@@ -1,9 +0,0 @@
-#include "parser.h"
-#define intswap(i) (i) = ((((i) >> 24) & 0xFF) | (((i) >> 8) & 0xFF00) | \
-	       (((i) << 8) &0xFF0000) | (((i) << 24) & 0xFF000000))
-char * flip_endian(unsigned char *, size_t, unsigned int);
-void swap_endian(unsigned char *buf, size_t n, unsigned int size);
-char * var_endian(Var *);
-
-void MSB(unsigned char * data, unsigned int data_elem, unsigned int word_size);
-void LSB(unsigned char * data, unsigned int data_elem, unsigned int word_size);
