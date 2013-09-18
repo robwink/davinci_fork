e07b30f593fb5d8348321e400717893bffd5fdd4
cedwards <cedwards@dedd6328-c50f-0410-8be5-b54a49638459>
2013-09-18 00:02:31 +0000

    added ubuntu build script for the isis3 module

---
+++ iomod_isis3/build34.ubuntu.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+echo Compiling ISIS3 module.... 3.4 version
+ISIS3DIR=/localdata/isis3/isis
+QTSRCDIR=/usr/include
+
+ARCH=64
+LD_RUN_PATH=$ISIS3DIR/3rdParty/lib
+echo $LD_RUN_PATH
+g++ -DHAVE_CONFIG_H -m$ARCH -fPIC -g -I.. -I../libltdl -I$ISIS3DIR/inc -I$QTSRCDIR -I/$QTSRCDIR/qt4/ -I/$QTSRCDIR/qt4/QtCore -Wall iomod_isis3.4.C -c -o iomod_isis34.o
+if [ $? != 0 ]; then
+    echo Compile failed.
+    exit 1
+fi
+echo Linking ISIS3 module...
+g++ -m$ARCH -fPIC -shared -Wl,-soname,isis34.dvio -Wl,-rpath=$ISIS3DIR/3rdParty/lib -o isis34.dvio iomod_isis34.o  -L$ISIS3DIR/lib -L$ISIS3DIR/3rdParty/lib -lisis3 -lkdu_a63R -lgsl -lgeos_c -lqwt -lxerces-c -lprotobuf -lQtDBus
+if [ $? != 0 ]; then
+    echo Link failed.
+    exit 1
+fi
+echo "Build of ISIS3 I/O module successful."
